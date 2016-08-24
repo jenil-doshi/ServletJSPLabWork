86d1eb4602153d110267f918c9b0d75b47316d17
jdoshi <jdoshi@visa.com>
2016-08-24 10:54:18 -0500

    Day6 Lab2

---
+++ Day6/src/Lab2/ApplyFeesStrategyBalance.java
@@ -0,0 +1,15 @@
+package Lab2;
+
+public class ApplyFeesStrategyBalance implements ApplyFeesStrategy {
+	private static final int LOW_BAL_FINE = 10;
+	private static final int MIN_BALANCE = 100;
+
+	@Override
+	public double calculateFees(int freqTransaction, double bal, int days) {
+		if (bal < MIN_BALANCE) {
+			return LOW_BAL_FINE;
+		} else {
+			return 0;
+		}
+	}
+}
